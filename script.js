fe35aacdede3f3c43dc130b9a8cad2dcc4d6ee7e
Eva-754 <eppathak@my.waketech.edu>
2026-03-19 16:58:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ function addToInventory(item){
 function checkCode(){
     try {
         //using regex to check code is only numbers
-        if (!/^\d+$/.test(code)) {
+        if (!/^\d+$/.test(try_code.value)) {
             throw new Error("Enter only numbers!");
         }
         if(try_code.value === "9999"){
